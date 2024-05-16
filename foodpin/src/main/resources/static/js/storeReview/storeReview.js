4f97fd5a16da89573f9a873b6f318db6945dbf1e
haechankim <haechan0202@naver.com>
2024-05-16 17:38:10 +0900

    리뷰 작성 완료 화면 구현 완료 리뷰 별점 구현 완료  해시태그 구현중

--- foodpin/src/main/resources/static/js/storeReview/storeReview.js
+++ foodpin/src/main/resources/static/js/storeReview/storeReview.js
@@ -11,5 +11,129 @@ selectMenu.addEventListener("click", ()=>{
   menuSection.classList.remove("popup-hidden");
 });
 
+/* ************************************************************* */
+const previewList = document.getElementsByClassName("preview");
+const inputImageList = document.getElementsByClassName("inputImage");
+const deleteImageList = document.getElementsByClassName("delete-image");
+
+
+const backupInputList = new Array(inputImageList.length);
+
+const changeImageFn = (inputImage, order) => {
+
+  const maxSize = 1024 * 1024 * 10;
+  const file = inputImage.files[0];
+
+  if(file == undefined){
+    console.log("파일 선택 취소됨");
+    const temp = backupInputList[order].cloneNode(true);
+
+    inputImage.after(temp); 
+    inputImage.remove(); 
+    inputImage = temp; 
+    
+    inputImage.addEventListener("change", e => {
+      changeImageFn(e.target, order);
+    });
+
+    return;
+  }
+
+  if(file.size > maxSize){
+    alert("10MB 이하의 이미지를 선택해주세요");
+
+    if(backupInputList[order] == undefined || backupInputList[order].value ==''){
+      inputImage.value=""; 
+      return;
+    }
+
+    const temp = backupInputList[order].cloneNode(true);
+
+    inputImage.after(temp);
+    inputImage.remove(); 
+    inputImage = temp; 
+    
+    inputImage.addEventListener("change", e => {
+      changeImageFn(e.target, order);
+    });
+
+    return;
+  }
+
+  const reader = new FileReader(); 
+  reader.readAsDataURL(file);
+  reader.addEventListener("load", e => {
+    const url = e.target.result;
+
+    previewList[order].src = url;
+
+    backupInputList[order] = inputImage.cloneNode(true);
+
+  });
+
+}
+
+for(let i=0 ; i<inputImageList.length ; i++){
+
+  inputImageList[i].addEventListener("change", e => {
+    changeImageFn(e.target, i);
+  })
+
+  deleteImageList[i].addEventListener("click", () => {
+
+    previewList[i].src = "";      
+    inputImageList[i].value  = ""; 
+    backupInputList[i].value = ""; 
+  });
+}
+
+/* ************************************************************************ */
+const ratingStars = [...document.getElementsByClassName("rating__star")];
+const ratingResult = document.querySelector(".rating__result");
+
+printRatingResult(ratingResult);
+
+function executeRating(stars, result) {
+   const starClassActive = "rating__star fas fa-star";
+   const starClassUnactive = "rating__star far fa-star";
+   const starsLength = stars.length;
+   let i;
+   stars.map((star) => {
+      star.onclick = () => {
+         i = stars.indexOf(star);
+
+         if (star.className.indexOf(starClassUnactive) !== -1) {
+            printRatingResult(result, i + 1);
+            for (i; i >= 0; --i) stars[i].className = starClassActive;
+         } else {
+            printRatingResult(result, i);
+            for (i; i < starsLength; ++i) stars[i].className = starClassUnactive;
+         }
+      };
+   });
+}
+
+function printRatingResult(result, num = 0) {
+   result.textContent = `${num} 점`;
+}
+
+executeRating(ratingStars, ratingResult);
+
+/* ************************************************************* */
+const keywordUl = document.querySelectorAll("#keywords");
+const keywords = document.querySelectorAll(".keyword-checkbox")
+
+for( let i = 0 ; i < keywords.length ; i++ ){
+  if(keywords[i].checked == true){
+    console.log("check");
+  } else{
+    console.log("not check");
+  }
+}
+
+
+
+
+
 
 
