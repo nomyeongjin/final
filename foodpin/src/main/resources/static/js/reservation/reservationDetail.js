35a404f7ad86d43c2e58d60f17ef70254400d0fa
dlwltn <03dreams@naver.com>
2024-05-17 17:26:04 +0900

    예약 페이지 화면 수정

--- foodpin/src/main/resources/static/js/reservation/reservationDetail.js
+++ foodpin/src/main/resources/static/js/reservation/reservationDetail.js
@@ -1,5 +1,3 @@
-// 알림창
-
 const storereservationbutton = document.querySelector("#storereservationbutton");
 
 if(storereservationbutton != null) {
@@ -17,7 +15,41 @@ noticeTitle.addEventListener("click", () => {
     const noticeInnerDetail = document.querySelector(".notice-inner-detail");
     
     noticeInnerDetail.classList.toggle("show-box");
-
-
 });
+
+
+//---------------------------
+
+// 예약 인원 수 체크
+const buttonItem = document.querySelectorAll(".button-item");
+
+for(let li of buttonItem) {
+
+    li.addEventListener("click", () =>{
+
+        for(let item of buttonItem){
+            item.classList.remove("select");
+s        }
+
+        li.classList.add("select");
+        console.log(li);
+    });
+};
+
+
+
+const timeItem = document.querySelectorAll(".time-item");
+
+for(let li of timeItem){
+
+    li.addEventListener("click", () => {
+
+        for(let item of timeItem){
+            item.classList.remove("select");
+        }
+
+        li.classList.add("select");
+    });
+};
+
 
