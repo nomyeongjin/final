9376494fe7b431922ebe014d90c0e1930b518621
hotncool <qqq366@naver.com>
2024-05-17 15:32:33 +0900

    manager mypage 구현중 . .

--- foodpin/src/main/java/com/project/foodpin/myPage/controller/ManagerMyPageController.java
+++ foodpin/src/main/java/com/project/foodpin/myPage/controller/ManagerMyPageController.java
@@ -1,6 +1,7 @@
 package com.project.foodpin.myPage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.project.foodpin.myPage.model.service.ManagerMyPageService;
@@ -16,7 +17,20 @@ public class ManagerMyPageController {
 	private final ManagerMyPageService serivce;
 	
 	
+	@GetMapping("storeEnroll")
+	public String managerEnroll() {
+		return "myPage/manager/storeEnroll";
+	}
 	
+	@GetMapping("reportReview")
+	public String storeInfo() {
+		return "myPage/manager/reportReview";
+	}
+	
+	@GetMapping("managerStoreInfo")
+	public String memberReportReview() {
+		return "myPage/manager/managerStoreInfo";
+	}
 	
 	
 	
