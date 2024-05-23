b5018ea491f3ca5839d277fe097b032d6dbee2d3
hotncool <qqq366@naver.com>
2024-05-23 17:32:32 +0900

    관리자 입점관리 수락 진행중

--- foodpin/src/main/java/com/project/foodpin/myPage/model/service/ManagerMyPageServiceImpl.java
+++ foodpin/src/main/java/com/project/foodpin/myPage/model/service/ManagerMyPageServiceImpl.java
@@ -1,7 +1,10 @@
 package com.project.foodpin.myPage.model.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.project.foodpin.member.model.dto.Member;
 import com.project.foodpin.myPage.model.mapper.ManagerMyPageMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -11,5 +14,17 @@ import lombok.RequiredArgsConstructor;
 public class ManagerMyPageServiceImpl implements ManagerMyPageService{
 	
 	private final ManagerMyPageMapper mapper;
+
+	// 가게 입점 내역 조회
+	@Override
+	public List<Member> storeRequestList(int memberCode, String memberStatus) {
+		return mapper.storeRequestList(memberCode, memberStatus);
+	}
+
+	// 가게 승인
+	@Override
+	public boolean approveMember(int memberNo) {
+		return mapper.approveMember(memberNo);
+	}
 
 }
