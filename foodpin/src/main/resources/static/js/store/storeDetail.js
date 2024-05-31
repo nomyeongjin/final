2531d4ced6725cc2c9e73f1e6b7c09c315eb6196
limhyunjo <sibagyeoni@gmail.com>
2024-05-31 17:37:18 +0900

    가게 상세 페이지 구현 미완

--- foodpin/src/main/resources/static/js/store/storeDetail.js
+++ foodpin/src/main/resources/static/js/store/storeDetail.js
@@ -106,6 +106,71 @@ window.onload = function() {
 /* ****************지도******************* */
 
 
+/**************** 가게 찜, 좋아요 개수 ******************/
+
+
+// 1. #bookmarkCheck 클릭 되었을 때
+const bookmarkCheck = document.querySelector("#bookmarkCheck");
+bookmarkCheck.addEventListener("click", e=>{
+
+  
+    // 3. 준비된 3개의 변수를 객체로 저장 -> (Json 변환 예정)
+    const obj = {
+        "memberNo" : loginMember,
+        "storeNo"  : storeNo,
+        "bookMark": bookMark
+    };
+
+    //4. 좋아요 INSERT / DELETE 비동기 요청
+    fetch("/store/like", {
+
+    method  : "POST",
+    headers : {"Content-Type" : "application/json"},
+    body    : JSON.stringify(obj) // 객체를 Json으로 문자화 
+
+    })
+
+    .then(resp =>resp.text()) // 반환 결과 text(글자) 형태로 변환
+    .then(count =>{
+
+        // count == 첫 번째 then의 파싱되어 반환된 값('-1' 또는 게시글 좋아요 수)
+        //console.log("result :", result);
+
+
+        if(count == -1){
+            console.log("좋아요 처리 실패");
+            return;
+        }
+
+        // 5. bookmark 값 0<->1 변환
+        // (왜? 클릭 될 때 마다 INSERT/DELETE 동작을 번갈아 가면서 할 수 있음)
+         bookMark = bookMark == 0? 1: 0;
+
+        // 6. 하트를 채웠다/비웠다 바꾸기
+        
+        e.target.classList.toggle("fa-regular");
+        e.target.classList.toggle("fa-solid");
+        
+
+
+        // 7. 게시글 좋아요 수 수정
+        e.target.nextElementSibling.innerText = count;
+
+        bookmarkCheck.classList.add('fa-bounce');
+
+        // 1초 후에 fa-shake 클래스를 제거
+        setTimeout(function () {
+          bookmarkCheck.classList.remove('fa-bounce');
+        }, 500);
+       
+
+    });
+
+});
+
+
+
+
 /* 가게 상세 설명 더보기 */
 const storeDetailContent =document.querySelector('.store-detail-content');
 const storeDetailText =document.querySelector('.store-detail-text');
@@ -258,13 +323,13 @@ shutScheduleInfoBtn.addEventListener("click",()=>{
 
 
 const storeMenuList = document.querySelector(".menu-image-container");
-const menus = storeMenuList.querySelectorAll(".menu");
+const menuB = storeMenuList.querySelectorAll(".menu-basic-list");
 const moreMenuImageBtn = document.querySelector("#moreMenuImageBtn");
 const shutMenuImageBtn = document.querySelector("#shutMenuImageBtn");
 
 // 처음에 3개의 메뉴를 표시
 let visibleMenus = 3;
-const totalMenus = menus.length;
+const totalMenus = menuB.length;
 
 showMenus();
 
@@ -282,10 +347,10 @@ shutMenuImageBtn.addEventListener("click", () => {
 function showMenus() {
   for (let i = 0; i < totalMenus; i++) {
     if (i < visibleMenus) {
-      menus[i].style.display = "flex";
-    } /* else {
-      menus[i].style.display = "none";
-    } */
+      menuB[i].style.display = "flex";
+    } else {
+      menuB[i].style.display = "none";
+    }
   }
 
   // 더보기/접기 버튼 토글
@@ -489,67 +554,5 @@ showReview.addEventListener("click", ()=>{
     behavior:'smooth'
   })
 })
-
-/**************** 가게 찜, 좋아요 개수 ******************/
-
-
-// 1. #bookmarkCheck 클릭 되었을 때
-const bookmarkCheck = document.querySelector("#bookmarkCheck");
-bookmarkCheck.addEventListener("click", e=>{
-
-  
-    // 3. 준비된 3개의 변수를 객체로 저장 -> (Json 변환 예정)
-    const obj = {
-        "memberNo" : loginMember,
-        "storeNo"  : storeNo,
-        "bookMark": bookMark
-    };
-
-    //4. 좋아요 INSERT / DELETE 비동기 요청
-    fetch("/store/like", {
-
-    method  : "POST",
-    headers : {"Content-Type" : "application/json"},
-    body    : JSON.stringify(obj) // 객체를 Json으로 문자화 
-
-    })
-
-    .then(resp =>resp.text()) // 반환 결과 text(글자) 형태로 변환
-    .then(count =>{
-
-        // count == 첫 번째 then의 파싱되어 반환된 값('-1' 또는 게시글 좋아요 수)
-        //console.log("result :", result);
-
-
-        if(count == -1){
-            console.log("좋아요 처리 실패");
-            return;
-        }
-
-        // 5. bookmark 값 0<->1 변환
-        // (왜? 클릭 될 때 마다 INSERT/DELETE 동작을 번갈아 가면서 할 수 있음)
-         bookMark = bookMark == 0? 1: 0;
-
-        // 6. 하트를 채웠다/비웠다 바꾸기
-        
-        e.target.classList.toggle("fa-regular");
-        e.target.classList.toggle("fa-solid");
-        
-
-
-        // 7. 게시글 좋아요 수 수정
-        e.target.nextElementSibling.innerText = count;
-
-        bookmarkCheck.classList.add('fa-bounce');
-
-        // 1초 후에 fa-shake 클래스를 제거
-        setTimeout(function () {
-          bookmarkCheck.classList.remove('fa-bounce');
-        }, 500);
-       
-
-    });
-
-});
 
 
