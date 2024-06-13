98a641b0a57ec71d798d4aaffd71902588712602
dlwltn <03dreams@naver.com>
2024-06-13 15:15:57 +0900

    모든 알림 기능 구현 완료

--- foodpin/src/main/java/com/project/foodpin/websocket/handler/NotiWebsocketHandler.java
+++ foodpin/src/main/java/com/project/foodpin/websocket/handler/NotiWebsocketHandler.java
@@ -318,31 +318,33 @@ public class NotiWebsocketHandler extends TextWebSocketHandler {
 				/* 예약 노쇼 
 				 * 1, 2회는 알림 발송
 				 * 3회는 문자 , 이메일 발송 */
-				
-				// 노쇼 횟수 조회
-				int reservNoshowCount =  service.reservNoshowCount();
 
 			// 예약 노쇼 알림(1회)
 			case "reservFirstNoshow":
+				
+				// memberNo
+				Member member = service.noshowMemberNo(notification.getPkNo());
+				reviewMemerNo = service.selectNoshowMemberNo(member.getMemberNo());
+				
 				contentForMember = String.format(
-						"<b>%s<b>님 <b>%s<b> 예약 날짜에 방문하지 않았습니다." + "/n" + "경고 > 노쇼 누적 1회 (노쇼 3회 처리 시 계정이 정지 됩니다.)",
-						sendMember.getMemberNickname(), notification.getReservDate());
-				urlForMember = "/myPage/member/reservation/noshow";
+						"<b>%s<b>님 <b>%s<b> 예약 날짜에 방문하지 않았습니다.<br>" + "경고 > 노쇼 누적 1회 (노쇼 3회 처리 시 계정이 정지 됩니다.)",
+						member.getMemberNickname(), notification.getReservDate());
+				urlForMember = "/myPage/member/reservation/cancelNoshow";
 
 				break;
 
 			// 예약 노쇼 알림(2회)
 			case "reservSecondNoshow":
+				
+				member = service.noshowMemberNo(notification.getPkNo());
+				reviewMemerNo = service.selectNoshowMemberNo(member.getMemberNo());
+				
 				contentForMember = String.format(
-						"<b>%s<b>님 <b>%s<b> 예약 날짜에 방문하지 않았습니다." + "/n" + "경고 > 노쇼 누적 2회 (노쇼 3회 처리 시 계정이 정지 됩니다.)");
-				urlForMember = "/myPage/member/reservation/noshow";
+						"<b>%s<b>님 <b>%s<b> 예약 날짜에 방문하지 않았습니다.<br>" + "경고 > 노쇼 누적 2회 (노쇼 3회 처리 시 계정이 정지 됩니다.)", 
+						member.getMemberNickname(), notification.getReservDate());
+				urlForMember = "/myPage/member/reservation/cancelNoshow";
 				break;
 				
-				// 예약 노쇼 알림(3회)
-			case "reservThirdNoshow" : 
-				
-				// 문자, 이메일 처리
-				break;
 				
 				// 리뷰 신고 ( 삭제 조치 )
 			case "reviewReportDeleteReview" : 
@@ -412,7 +414,7 @@ public class NotiWebsocketHandler extends TextWebSocketHandler {
 				contentForStore = String.format("<b>%s<b>이 <b>%s<b> 방문 후 후기를 남겨 주셨습니다.", reservation.getMemberNickname(),
 						reservation.getReservDate() + " " + reservation.getReservTime());
 
-				urlForStore = "/myPage/store/review";
+				urlForStore = "/myPage/store/reviewUnanswered";
 
 				notiCode = 5;
 				break;
