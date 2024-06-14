622d42505c861db75fbe3dd3df35dacac146df8c
hotncool <qqq366@naver.com>
2024-06-14 17:43:37 +0900

    css 수정 사이드 추가

--- foodpin/src/main/resources/static/js/myPage/store/storeOff.js
+++ foodpin/src/main/resources/static/js/myPage/store/storeOff.js
@@ -325,6 +325,10 @@ const createPopup = (info) => {
       popupFrm.remove();
    })
 
+   popupFrm.addEventListener("click", (event) => {
+      event.stopPropagation();
+   });
+
    // 팝업창 외 다른 클릭 이벤트 방지
 };
 
