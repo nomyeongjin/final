cb91d2d051f761b115feeca3ab0b3f75f274d828
user1 <cicadax31@gmail.com>
2024-06-17 14:28:27 +0900

    Revert "가게 마이페이지 오류 수정"
    This reverts commit ca0ba29c3d233f9486231686a556a50ab935bf83.

--- foodpin/src/main/resources/static/js/store/storeOpen.js
+++ foodpin/src/main/resources/static/js/store/storeOpen.js
@@ -39,34 +39,39 @@ document.addEventListener('DOMContentLoaded', function () {
         const storeNo = window.location.href.split('/').slice(-1)[0]; // 주소에서 storeNo 받아오기
         // console.log(storeNo);
 
-        const response = await fetch('/store/storeOpen?storeNo=' + storeNo); // fetch
+        const response = await fetch(`/store/storeOpen?storeNo=${storeNo}`); // fetch
         const map = await response.json();
         // console.log(map);
         // console.log(map.offWeek);
 
         const schedule = []; // 영업 시간 담을 배열
-        let offWeekArr = map.offWeek.slice(0, -1).split('/');
+
+        let offWeekArr = [];
+        offWeekArr.push(map.offWeek.slice(0, -1).split('/'));
+        // console.log(offWeekArr);
+
+        // span에 영업 상태 표시
+        if (offWeekArr.includes(currentDay)) storeStatusText.innerText = "휴무일";
+        else storeStatusText.innerText = "영업일";
 
         let openClose = "";
         let breaktime = "";
-
-        if(map.openBreak.openHour == '00:00') openClose = "종일";
-        else openClose = map.openBreak.openHour + " - " + map.openBreak.closeHour;
-
-        if(map.openBreak.breaktimeStart == null || map.openBreak.breaktimeStart == '00:00') breaktime = "없음";
-        else breaktime = map.openBreak.breaktimeStart + " - " + map.openBreak.breaktimeEnd;
 
         for (let i = 0; i < 7; i++) { // 요일별행에 날짜-요일-영업시간-브레이크타임 추가하는 반복문 (0은 오늘 기준)
             const dayIndex = (currentDay + i) % 7; // dayIndex : 0:일요일 ~ 6:토요일
             const day = daysOfWeek[dayIndex]; // day : 해당 요일의 이름
 
-            // span에 영업 상태 표시
-            if(offWeekArr.includes(String(currentDay))) storeStatusText.innerText = "휴무일";
-            else storeStatusText.innerText = "영업일";
 
             const date = new Date(currentDate);
             date.setDate(currentDate.getDate() + i); // date : 해당 날짜
             const dateStr = `${date.getMonth() + 1}.${date.getDate()}`; // 날짜를 "월.일" 형식의 문자열로 변환
+
+            if(map.openBreak.openHour == '00:00') openClose = "종일";
+            else openClose = map.openBreak.openHour + " - " + map.openBreak.closeHour;
+
+            if(map.openBreak.breaktimeStart == '00:00') breaktime = "없음";
+            else breaktime = map.openBreak.breaktimeStart + " - " + map.openBreak.breaktimeEnd;
+
 
             schedule.push({
                 day: dayIndex === currentDay ? `[오늘] ${dateStr} ${day}` : `${dateStr} ${day}`,
